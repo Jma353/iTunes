9b9edfc356b39986be972e2e266e0d1414b14082
Jma353 <jma353@cornell.edu>
2016-12-18 16:57:48 -0500

    Progress with networking

---
+++ src/main/java/HTTP.java
@@ -0,0 +1,50 @@
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.HttpClientBuilder;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URI;
+
+/**
+ * Abstract class defining HTTP methods
+ */
+public abstract class HTTP {
+
+  /**
+   * Send a simple GET request
+   * @param uri - URI
+   */
+  protected void get (URI uri) throws IOException {
+
+    /* Client & request setup */
+    HttpClient client = HttpClientBuilder.create().build();
+    HttpGet request = new HttpGet(uri.toString());
+
+    /* Make the request */
+    HttpResponse response = client.execute(request);
+
+    /* Check response code */
+    System.out.println("Response Code : "
+      + response.getStatusLine().getStatusCode());
+
+    /* Stream */
+    BufferedReader rd = new BufferedReader(
+      new InputStreamReader(response.getEntity().getContent()));
+
+    /* Build result string */
+    StringBuffer result = new StringBuffer();
+    String line;
+    while ((line = rd.readLine()) != null) {
+      result.append(line);
+    }
+
+    /* Print result */
+    System.out.println(result);
+
+  }
+
+
+}
--- src/main/java/iTunes.java
+++ src/main/java/iTunes.java
@@ -1,3 +1,7 @@
+import org.apache.http.client.utils.URIBuilder;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Locale;
@@ -6,7 +10,10 @@ import java.util.Locale;
  * Main class to handle making API Requests.
  * In the form of a singleton.
  */
-public class iTunes {
+public class iTunes extends HTTP {
+
+  /* Exception specific to iTunes driver */
+  public class iTunesException extends Exception {}
 
   /* Singleton logic */
   private static iTunes instance = null;
@@ -25,18 +32,75 @@ public class iTunes {
    * Protected constructor (only used in singleton logic)
    */
   protected iTunes () {
-    /* Base URL */
-    this.baseURL = "https://itunes.apple.com/";
-    /* Set of valid country ISO codes */
+    this.baseURL = "https://itunes.apple.com";
     this.countryISOs =
       new HashSet<String> (Arrays.asList(Locale.getISOCountries()));
   }
 
-  /* Hand-tests */
-  public static void main (String[] args) {
+  /**
+   * Base URL getter
+   * @return - String
+   */
+  private String getBaseURL() {
+    return baseURL;
+  }
+
+  /**
+   * Country ISO set getter
+   * @return - HashSet of String
+   */
+  private HashSet<String> getCountryISOs() {
+    return countryISOs;
+  }
+
+  /**
+   * Search URI builder
+   * @return - URIBuilder
+   * @throws URISyntaxException
+   */
+  public URIBuilder searchURIBuilderBase () throws URISyntaxException {
+    return new URIBuilder(getBaseURL() + "/search");
+  }
+
+  /**
+   * Lookup URI builder
+   * @return - URIBuilder
+   * @throws URISyntaxException
+   */
+  public URIBuilder lookupURIBuilderBase () throws URISyntaxException {
+    return new URIBuilder(getBaseURL() + "/lookup");
+  }
+
+  /**
+   * Search by term
+   * @param term - String
+   */
+  public void search (String term) {
+    try {
+      URIBuilder uriBuilder = searchURIBuilderBase();
+      uriBuilder.addParameter("term", term);
+      get(uriBuilder.build());
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+
+  /**
+   * Search by term & country ISO code
+   * @param term - String
+   * @param iso - String
+   * @throws iTunesException
+   */
+  public void search (String term,
+                      String iso) throws iTunesException {
     // TODO
   }
 
-
+  /* Hand-tests */
+  public static void main (String[] args) {
+    iTunes.getInstance().search("lol");
+  }
 
 }
+
+
