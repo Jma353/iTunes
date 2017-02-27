78893a5cc14294711537c6eb39b19f9d9dcefecf
Jma353 <jma353@cornell.edu>
2017-02-27 15:13:20 -0500

    Refactor RSS stream operation

--- src/main/java/com/github/Jma353/itunes/HTTP.java
+++ src/main/java/com/github/Jma353/itunes/HTTP.java
@@ -13,6 +13,8 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URI;
+import java.net.URL;
+import java.net.URLConnection;
 
 /**
  * Abstract class defining itunes.HTTP methods
@@ -62,17 +64,14 @@ public class HTTP {
    */
   public static BufferedReader getResponseRSS(CloseableHttpClient httpClient,
                                               String dataSource) throws IOException {
-    HttpGet httpGet = new HttpGet(dataSource);
-    httpGet.addHeader("User-Agent", userAgentValue);
-    CloseableHttpResponse httpResponse = httpClient.execute(httpGet);
-    BufferedReader reader;
 
-    if (httpResponse.getStatusLine().getStatusCode() == HttpStatus.SC_OK) {
-      reader = new BufferedReader(new InputStreamReader(httpResponse.getEntity().getContent()));
-    } else {
-      reader = null;
-    }
-    return reader;
+    URL src = new URL(dataSource);
+    URLConnection srcConn = src.openConnection();
+    srcConn.setRequestProperty("User-Agent", userAgentValue);
+    BufferedReader reader;
+    BufferedReader in =
+      new BufferedReader(new InputStreamReader(srcConn.getInputStream(), "UTF-8"));
+    return in;
   }
 
 
